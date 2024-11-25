a90d8f72b676545bda432c45d85ee7373fe37030
Franklin <43481761+Franklin-Barbosa@users.noreply.github.com>
2024-11-25 00:05:37 -0300

    feat: update Q12.asm

--- Lista04/Q12.asm
+++ Lista04/Q12.asm
@@ -19,36 +19,35 @@ main:
     syscall
 
 triangulo:
-    add $25 $4 $0
-    addi $8 $0 2
-    mul $8 $4 $8 # maximo
-    addi $9 $0 1 # i
+    add $25 $4 $0	# $4 para $25, tamanho do triangulo
+    addi $8 $0 2	# maximo
+    mul $8 $4 $8 	# armazena o dobro do tamanho original do triangulo
+    addi $9 $0 1 	# i
     addi $16 $0 2
 primeiro_laco:
-    beq $9 $8 fim_primeiro_laco
-    add $10 $0 $0 # j
+    beq $9 $8 fim_primeiro_laco 	# se $9 == $8 o loop externo termina
+    add $10 $0 $0 	# j
     add $15 $9 $0
-    slt $20 $25 $15
-    bne $20 $0 subtrai
-    j segundo_laco
+    slt $20 $25 $15 	# se $25 < $15, resultado (0 ou 1) vai para $20
+    bne $20 $0 subtrai	# se $20 != $0, salta para subtrai
+    j segundo_laco	# senão salta para segundo_laco
 subtrai:
-    sub $15 $15 $16
-    addi $16 $16 2
+    sub $15 $15 $16 
+    addi $16 $16 2	# incrementa $16 em 2
 segundo_laco:
-    beq $10 $15 fim_segundo_laco
-    add $4 $0 '*'
-    
+    beq $10 $15 fim_segundo_laco 	# se $10 == $15, termina o loop interno
+    add $4 $0 '*'	# imprime *
     addi $2 $0 11
     syscall
     
-    addi $10 $10 1
+    addi $10 $10 1	# incrementa $10
     j segundo_laco
 fim_segundo_laco:
-    addi $4 $0 '\n'
+    addi $4 $0 '\n'	# quebra de linha
     addi $2 $0 11
     syscall
     
-    addi $9 $9 1
+    addi $9 $9 1	# incrementa $9
     j primeiro_laco
 fim_primeiro_laco:
     jr $31
